7c4fd8117aa3d419d58528312aa60ab563677375
OC <30299179+behindthedash@users.noreply.github.com>
2026-08-22 18:54:02 -0700

    feat(ui): export compact window primitives

---
+++ src/interview_copilot/ui/__init__.py
@@ -0,0 +1,25 @@
+from .windowing import (
+    CompactWindowConfig,
+    CompactWindowController,
+    GeometryStore,
+    WindowGeometry,
+    WindowSurface,
+    WorkArea,
+    clamp_opacity,
+    is_geometry_visible,
+    recover_geometry,
+    system_work_areas,
+)
+
+__all__ = [
+    "CompactWindowConfig",
+    "CompactWindowController",
+    "GeometryStore",
+    "WindowGeometry",
+    "WindowSurface",
+    "WorkArea",
+    "clamp_opacity",
+    "is_geometry_visible",
+    "recover_geometry",
+    "system_work_areas",
+]
